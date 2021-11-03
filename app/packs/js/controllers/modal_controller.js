adf572fe99b713b5b06062a14afa03c8be3e3570
Cernan Bernardo <cernanb@gmail.com>
2021-11-02 22:31:24 -0700

    Update modal controller

--- app/packs/js/controllers/modal_controller.js
+++ app/packs/js/controllers/modal_controller.js
@@ -1,11 +1,14 @@
-import { Controller } from 'stimulus'
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['modal']
+  static targets = ["modal"];
 
   close() {
-    this.modalTarget.remove()
-
-    document.dispatchEvent(new Event('actions-modal:close'))
+    this.modalTarget.remove();
+    document.getElementById("attach_modal").src = undefined;
+    document.getElementById("actions_show").src = undefined;
+    document.getElementById("destroy_attachment_form").src = undefined;
+    document.getElementById("alerts").src = undefined;
+    document.dispatchEvent(new Event("actions-modal:close"));
   }
 }
